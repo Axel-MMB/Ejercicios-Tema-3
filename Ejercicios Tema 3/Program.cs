4eab9f0b10300dfbdcb45912e27fe93a00cc2c92
Axel M <axelmunoa@gmail.com>
2022-09-26 17:20:07 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicios Tema 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicios_Tema_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = suma(3, 4, 5);
+            Console.WriteLine(total);
+            Console.ReadKey();
+            int suma(int numero1, int numero2, int numero3)
+            {
+                return numero1 + numero2 + numero3;
+            }
+
+            Coche miCoche = new Coche();
+            miCoche.incremetarPuertas();
+            Console.WriteLine(miCoche.numeroPuertas);
+            Console.ReadKey();
+        }
+
+        class Coche
+        {
+            public int numeroPuertas = 0;
+            public void incremetarPuertas()
+            {
+                this.numeroPuertas++;
+
+            }
+        }
+    }
+}
